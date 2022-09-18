b0ec458a5d248e8ae8d4205efc153cb96cf6d372
Aldar <aldarkonov@mail.ru>
2022-09-19 01:03:56 +0300

    feat: add TodoInput component

---
+++ src/components/TodoInput/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.css';
+
+interface TodoInputProps {
+  value: string,
+  onChange: () => void
+}
+
+const TodoInput: React.FC<TodoInputProps> = ({ value, onChange }) => {
+  return (
+    <div className='todo__input-wrapper'>
+      {/* <i className='todo__icon'></i> */}
+      <input
+        className='todo__input'
+        type="text"
+        placeholder='Click to add task'
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default TodoInput;
